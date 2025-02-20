d26647197223e7301be4747078d782d9ae3eb46a
refff <fedorv59@gmail.com>
2025-02-20 10:17:38 +0300

    delete useless import's

--- task/src/taskmanagement/presentation/UserController.java
+++ task/src/taskmanagement/presentation/UserController.java
@@ -3,27 +3,14 @@ package taskmanagement.presentation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.*;
 import taskmanagement.domain.AppUser;
 import taskmanagement.domain.Task;
 import taskmanagement.serviceLayer.AuthService;
 import taskmanagement.serviceLayer.UserService;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Base64;
-import java.util.List;
 import java.util.Map;
 
 @RestController
